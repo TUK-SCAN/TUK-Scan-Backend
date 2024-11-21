84f5c664d4904742d92e2f010a411404736a39de
GiHwan2 <sandwich5011@naver.com>
2024-11-21 21:19:14 +0900

    chore: SpringBoot Project 초기 세팅

---
+++ src/main/java/com/tukscan/tukscan/TukscanApplication.java
@@ -0,0 +1,13 @@
+package com.tukscan.tukscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TukscanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tukscan/tukscan/TukscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tukscan.tukscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
