155555fb5a65463da00f16f0213bbbcb822a751b
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-01-02 07:18:06 +0900

    chore: Enable Annotation 추가

--- src/main/java/com/tukscan/tukscan/TukscanApplication.java
+++ src/main/java/com/tukscan/tukscan/TukscanApplication.java
@@ -2,11 +2,14 @@ package com.tukscan.tukscan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableJpaAuditing
+@EnableAsync
+@EnableJpaRepositories(basePackages = "com.tukscan.tukscan.*.repository.mysql")
+@EnableRedisRepositories(basePackages = "com.tukscan.tukscan.*.repository.redis")
+@SpringBootApplication
 public class TukscanApplication {
 
 	public static void main(String[] args) {
