16db695a707cd7f6caf3250a0f6b98dfeacb99b9
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-01-04 23:54:39 +0900

    feat/#6: OrderMemo 구현

---
+++ src/main/java/com/tookscan/tookscan/order/domain/OrderMemo.java
@@ -0,0 +1,43 @@
+package com.tookscan.tookscan.order.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "order_memos")
+public class OrderMemo {
+
+    /* -------------------------------------------- */
+    /* Default Column ----------------------------- */
+    /* -------------------------------------------- */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /* -------------------------------------------- */
+    /* Information Column ------------------------- */
+    /* -------------------------------------------- */
+    @Column(name = "memo", length = 500, nullable = false)
+    private String memo;
+
+    /* -------------------------------------------- */
+    /* Many to One Column ------------------------- */
+    /* -------------------------------------------- */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    /* -------------------------------------------- */
+    /* Methods ------------------------------------ */
+    /* -------------------------------------------- */
+    @Builder
+    public OrderMemo(String memo, Order order) {
+        this.memo = memo;
+        this.order = order;
+    }
+}
