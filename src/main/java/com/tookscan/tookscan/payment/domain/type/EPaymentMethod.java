81e2cf98cf83b94055407e2f9ef315dd0b203fe5
GiHwan2 <sandwich5011@naver.com>
2025-01-12 23:00:20 +0900

    refactor: PaymentMethod 오타 수정

--- src/main/java/com/tookscan/tookscan/payment/domain/type/EPaymentMethod.java
+++ src/main/java/com/tookscan/tookscan/payment/domain/type/EPaymentMethod.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum EPaymentMethod {
     CARD("카드"),
-    VERTUAL_ACCOUNT("가상계좌"),
+    VIRTUAL_ACCOUNT("가상계좌"),
     EASY_PAYMENT("간편결제"),
     ACCOUNT_TRANSFER("계좌이체"),
     GIFT_VOUCHER("상품권");
@@ -17,7 +17,7 @@ public enum EPaymentMethod {
     public static EPaymentMethod fromString(String value) {
         return switch (value.toUpperCase()) {
             case "CARD" -> CARD;
-            case "VERTUAL_ACCOUNT" -> VERTUAL_ACCOUNT;
+            case "VIRTUAL_ACCOUNT" -> VIRTUAL_ACCOUNT;
             case "EASY_PAYMENT" -> EASY_PAYMENT;
             case "ACCOUNT_TRANSFER" -> ACCOUNT_TRANSFER;
             case "GIFT_VOUCHER" -> GIFT_VOUCHER;
