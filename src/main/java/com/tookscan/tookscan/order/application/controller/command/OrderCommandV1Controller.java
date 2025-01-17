d8334cd03c9d63d58386bbe05bef24d50a22517f
GiHwan2 <sandwich5011@naver.com>
2025-01-18 01:54:35 +0900

    del: delete unnecessary code

--- src/main/java/com/tookscan/tookscan/order/application/controller/command/OrderCommandV1Controller.java
+++ src/main/java/com/tookscan/tookscan/order/application/controller/command/OrderCommandV1Controller.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
