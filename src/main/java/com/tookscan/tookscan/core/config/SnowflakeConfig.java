a3a32eb98460de179fb66ba36fd590bb9fda9cfc
GiHwan2 <sandwich5011@naver.com>
2025-01-21 00:06:18 +0900

    feat: Snowflake 고유 ID 생성 클래스 구현

---
+++ src/main/java/com/tookscan/tookscan/core/config/SnowflakeConfig.java
@@ -0,0 +1,26 @@
+package com.tookscan.tookscan.core.config;
+
+import com.tookscan.tookscan.core.utility.Snowflake;
+import java.time.Instant;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SnowflakeConfig {
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${snowflake.node-id}")
+    private long nodeId;
+
+    @Value("${snowflake.epoch}")
+    private String epoch;
+    @Bean
+    public Snowflake snowflake() {
+        long customEpoch = Instant.parse(epoch).toEpochMilli();
+        return new Snowflake(nodeId, customEpoch);
+    }
+}
