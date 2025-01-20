312c30e82b203688e0f35dbcc69eccfb750421e8
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-01-20 20:23:40 +0900

    chore: 충돌 해결

--- src/main/java/com/tookscan/tookscan/address/dto/request/AddressRequestDto.java
+++ src/main/java/com/tookscan/tookscan/address/dto/request/AddressRequestDto.java
@@ -3,9 +3,7 @@ package com.tookscan.tookscan.address.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tookscan.tookscan.address.domain.Address;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 
 @Builder
