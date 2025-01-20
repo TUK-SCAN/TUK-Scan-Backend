014a03e9cbe6ce2c76a6bab0f2705558a81a4018
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-01-21 00:46:44 +0900

    chore: 주석 수정

--- src/main/java/com/tookscan/tookscan/security/application/usecase/IssueAuthenticationCodeUseCase.java
+++ src/main/java/com/tookscan/tookscan/security/application/usecase/IssueAuthenticationCodeUseCase.java
@@ -7,9 +7,9 @@ import com.tookscan.tookscan.security.application.dto.response.IssueAuthenticati
 @UseCase
 public interface IssueAuthenticationCodeUseCase {
     /**
-     * 이메일 인증 코드 발급
-     * @param requestDto 이메일 인증 요청 DTO
-     * @return 이메일 인증 응답 DTO
+     * 휴대폰 인증 코드 발급
+     * @param requestDto 휴대폰 인증 요청 DTO
+     * @return 휴대폰 인증 응답 DTO
      */
     IssueAuthenticationCodeResponseDto execute(IssueAuthenticationCodeRequestDto requestDto);
 }
