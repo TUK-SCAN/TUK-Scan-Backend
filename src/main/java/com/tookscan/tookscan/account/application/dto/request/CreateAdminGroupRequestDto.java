ded36611db227b1a8679f12c3d448e64f6b3f664
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-01-29 23:58:28 +0900

    chore/#57: NotNull 제거

--- src/main/java/com/tookscan/tookscan/account/application/dto/request/CreateAdminGroupRequestDto.java
+++ src/main/java/com/tookscan/tookscan/account/application/dto/request/CreateAdminGroupRequestDto.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotNull;
 
 public record CreateAdminGroupRequestDto(
         @JsonProperty("name")
-        @NotNull(message = "이름은 필수입니다")
         @NotBlank(message = "이름은 필수입니다")
         String name
 ) {
