b1000f6c51ba1d914f80c7ed1869a2fac1e01de8
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-01-29 22:39:58 +0900

    refactor/#61: GroupRepository 추상화

--- src/main/java/com/tookscan/tookscan/account/application/service/CreateAdminUserGroupService.java
+++ src/main/java/com/tookscan/tookscan/account/application/service/CreateAdminUserGroupService.java
@@ -7,11 +7,9 @@ import com.tookscan.tookscan.account.domain.Group;
 import com.tookscan.tookscan.account.domain.User;
 import com.tookscan.tookscan.account.domain.UserGroup;
 import com.tookscan.tookscan.account.domain.service.UserGroupService;
+import com.tookscan.tookscan.account.repository.GroupRepository;
 import com.tookscan.tookscan.account.repository.UserGroupRepository;
 import com.tookscan.tookscan.account.repository.UserRepository;
-import com.tookscan.tookscan.account.repository.mysql.GroupJpaRepository;
-import com.tookscan.tookscan.core.exception.error.ErrorCode;
-import com.tookscan.tookscan.core.exception.type.CommonException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +23,7 @@ import java.util.UUID;
 public class CreateAdminUserGroupService implements CreateAdminUserGroupUseCase {
 
     private final UserGroupRepository userGroupRepository;
-    private final GroupJpaRepository groupJpaRepository;
+    private final GroupRepository groupRepository;
     private final UserRepository userRepository;
 
     private final UserGroupService userGroupService;
@@ -39,7 +37,7 @@ public class CreateAdminUserGroupService implements CreateAdminUserGroupUseCase 
         List<UserGroup> userGroups = objectPairs.stream()
                 .map(pair -> {
                     User user = userRepository.findByIdOrElseThrow(pair.getLeft());
-                    Group group = groupJpaRepository.findById(pair.getRight()).orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_RESOURCE));
+                    Group group = groupRepository.findByIdOrElseThrow(pair.getRight());
 
                     return userGroupService.createUserGroup(user, group);
                 })
