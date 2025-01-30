a3636960f892e3b3c2943107eba2563bfdbafa44
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-01-30 21:36:39 +0900

    feat/#83: 8.2.2 약관 조회 API 구현

---
+++ src/main/java/com/tookscan/tookscan/term/application/service/ReadUserTermOverviewService.java
@@ -0,0 +1,24 @@
+package com.tookscan.tookscan.term.application.service;
+
+import com.tookscan.tookscan.term.application.dto.response.ReadUserTermOverviewResponseDto;
+import com.tookscan.tookscan.term.application.usecase.ReadUserTermOverviewUseCase;
+import com.tookscan.tookscan.term.domain.type.ETermType;
+import com.tookscan.tookscan.term.repository.TermRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ReadUserTermOverviewService implements ReadUserTermOverviewUseCase {
+
+    private final TermRepository termRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public ReadUserTermOverviewResponseDto execute(String type) {
+        ETermType eType = ETermType.fromString(type);
+
+        return ReadUserTermOverviewResponseDto.fromEntities(termRepository.findAllByType(eType));
+    }
+}
