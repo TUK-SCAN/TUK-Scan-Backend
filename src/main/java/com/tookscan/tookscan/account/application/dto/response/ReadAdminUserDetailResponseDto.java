deb72335e2dfb3ddecd7d5da27c2f996d2dda843
GiHwan2 <sandwich5011@naver.com>
2025-01-30 22:40:00 +0900

    refactor/#80: AddressResponseDto로 통합

--- src/main/java/com/tookscan/tookscan/account/application/dto/response/ReadAdminUserDetailResponseDto.java
+++ src/main/java/com/tookscan/tookscan/account/application/dto/response/ReadAdminUserDetailResponseDto.java
@@ -2,7 +2,7 @@ package com.tookscan.tookscan.account.application.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tookscan.tookscan.account.domain.User;
-import com.tookscan.tookscan.address.domain.Address;
+import com.tookscan.tookscan.address.dto.response.AddressResponseDto;
 import com.tookscan.tookscan.core.dto.SelfValidating;
 import com.tookscan.tookscan.security.domain.type.ESecurityProvider;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -38,14 +38,15 @@ public class ReadAdminUserDetailResponseDto extends SelfValidating<ReadAdminUser
 
     @JsonProperty("address")
     @Schema(description = "주소")
-    private final AddressDto address;
+    private final AddressResponseDto address;
 
     @JsonProperty("memo")
     @Schema(description = "메모", example = "VIP 고객")
     private final String memo;
 
     @Builder
-    public ReadAdminUserDetailResponseDto(String serialId, String name, ESecurityProvider provider, String phoneNumber, String email, AddressDto address, String memo) {
+    public ReadAdminUserDetailResponseDto(String serialId, String name, ESecurityProvider provider, String phoneNumber,
+                                          String email, AddressResponseDto address, String memo) {
         this.serialId = serialId;
         this.name = name;
         this.provider = provider;
@@ -56,74 +57,6 @@ public class ReadAdminUserDetailResponseDto extends SelfValidating<ReadAdminUser
         this.validateSelf();
     }
 
-    @Getter
-    public static class AddressDto {
-
-        @JsonProperty("address_name")
-        @Schema(description = "주소명", example = "서울시 강남구 역삼동 역삼로1길")
-        @NotNull
-        private final String addressName;
-
-        @JsonProperty("region_1depth_name")
-        @Schema(description = "1차 지역명", example = "서울시")
-        @NotNull
-        private final String region1DepthName;
-
-        @JsonProperty("region_2depth_name")
-        @Schema(description = "2차 지역명", example = "강남구")
-        @NotNull
-        private final String region2DepthName;
-
-        @JsonProperty("region_3depth_name")
-        @Schema(description = "3차 지역명", example = "역삼동")
-        @NotNull
-        private final String region3DepthName;
-
-        @JsonProperty("region_4depth_name")
-        @Schema(description = "4차 지역명", example = "역삼로1길")
-        private final String region4DepthName;
-
-        @JsonProperty("address_detail")
-        @Schema(description = "상세 주소", example = "3층 301호")
-        @NotNull
-        private final String addressDetail;
-
-        @JsonProperty("longitude")
-        @Schema(description = "경도", example = "127.123456")
-        @NotNull
-        private final Double longitude;
-
-        @JsonProperty("latitude")
-        @Schema(description = "위도", example = "37.123456")
-        @NotNull
-        private final Double latitude;
-
-        @Builder
-        public AddressDto(String addressName, String region1DepthName, String region2DepthName, String region3DepthName, String region4DepthName, String addressDetail, Double longitude, Double latitude) {
-            this.addressName = addressName;
-            this.region1DepthName = region1DepthName;
-            this.region2DepthName = region2DepthName;
-            this.region3DepthName = region3DepthName;
-            this.region4DepthName = region4DepthName;
-            this.addressDetail = addressDetail;
-            this.longitude = longitude;
-            this.latitude = latitude;
-        }
-
-        public static AddressDto fromEntity(Address address) {
-            return AddressDto.builder()
-                    .addressName(address.getAddressName())
-                    .region1DepthName(address.getRegion1DepthName())
-                    .region2DepthName(address.getRegion2DepthName())
-                    .region3DepthName(address.getRegion3DepthName())
-                    .region4DepthName(address.getRegion4DepthName())
-                    .addressDetail(address.getAddressDetail())
-                    .longitude(address.getLongitude())
-                    .latitude(address.getLatitude())
-                    .build();
-        }
-    }
-
     public static ReadAdminUserDetailResponseDto fromEntity(User user) {
         return ReadAdminUserDetailResponseDto.builder()
                 .serialId(user.getSerialId())
@@ -131,7 +64,8 @@ public class ReadAdminUserDetailResponseDto extends SelfValidating<ReadAdminUser
                 .provider(user.getProvider())
                 .phoneNumber(user.getPhoneNumber())
                 .email(user.getEmail() != null ? user.getEmail() : null)
-                .address(AddressDto.fromEntity(user.getAddress()) != null ? AddressDto.fromEntity(user.getAddress()) : null)
+                .address(AddressResponseDto.fromEntity(user.getAddress()) != null ? AddressResponseDto.fromEntity(
+                        user.getAddress()) : null)
                 .memo(user.getMemo() != null ? user.getMemo() : null)
                 .build();
     }
--- src/main/java/com/tookscan/tookscan/account/application/dto/response/ReadUserUserDetailResponseDto.java
+++ src/main/java/com/tookscan/tookscan/account/application/dto/response/ReadUserUserDetailResponseDto.java
@@ -2,7 +2,7 @@ package com.tookscan.tookscan.account.application.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tookscan.tookscan.account.domain.User;
-import com.tookscan.tookscan.address.domain.Address;
+import com.tookscan.tookscan.address.dto.response.AddressResponseDto;
 import com.tookscan.tookscan.core.dto.SelfValidating;
 import com.tookscan.tookscan.security.domain.type.ESecurityProvider;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -38,10 +38,11 @@ public class ReadUserUserDetailResponseDto extends SelfValidating<ReadUserUserDe
 
     @JsonProperty("address")
     @Schema(description = "주소")
-    private AddressDto address;
+    private AddressResponseDto address;
 
     @Builder
-    public ReadUserUserDetailResponseDto(String name, ESecurityProvider provider, String serialId, String phoneNumber, String email, AddressDto address) {
+    public ReadUserUserDetailResponseDto(String name, ESecurityProvider provider, String serialId, String phoneNumber,
+                                         String email, AddressResponseDto address) {
         this.name = name;
         this.provider = provider;
         this.serialId = serialId;
@@ -51,79 +52,6 @@ public class ReadUserUserDetailResponseDto extends SelfValidating<ReadUserUserDe
         this.validateSelf();
     }
 
-    @Getter
-    public static class AddressDto extends SelfValidating<AddressDto> {
-
-        @JsonProperty("address_name")
-        @Schema(description = "주소명", example = "서울시 강남구 역삼동 역삼로1길")
-        @NotNull(message = "주소명은 필수입니다")
-        private final String addressName;
-
-        @JsonProperty("region_1depth_name")
-        @Schema(description = "1차 지역명", example = "서울시")
-        @NotNull(message = "1차 지역명은 필수입니다")
-        private final String region1DepthName;
-
-        @JsonProperty("region_2depth_name")
-        @Schema(description = "2차 지역명", example = "강남구")
-        @NotNull(message = "2차 지역명은 필수입니다")
-        private final String region2DepthName;
-
-        @JsonProperty("region_3depth_name")
-        @Schema(description = "3차 지역명", example = "역삼동")
-        @NotNull(message = "3차 지역명은 필수입니다")
-        private final String region3DepthName;
-
-        @JsonProperty("region_4depth_name")
-        @Schema(description = "4차 지역명", example = "역삼로1길")
-        private final String region4DepthName;
-
-        @JsonProperty("address_detail")
-        @Schema(description = "상세 주소", example = "3층 301호")
-        @NotNull(message = "상세 주소는 필수입니다")
-        private final String addressDetail;
-
-        @JsonProperty("longitude")
-        @Schema(description = "경도", example = "127.123456")
-        @NotNull(message = "경도는 필수입니다")
-        private final Double longitude;
-
-        @JsonProperty("latitude")
-        @Schema(description = "위도", example = "37.123456")
-        @NotNull(message = "위도는 필수입니다")
-        private final Double latitude;
-
-        @Builder
-        public AddressDto(String addressName, String region1DepthName, String region2DepthName, String region3DepthName, String region4DepthName, String addressDetail, Double longitude, Double latitude) {
-            this.addressName = addressName;
-            this.region1DepthName = region1DepthName;
-            this.region2DepthName = region2DepthName;
-            this.region3DepthName = region3DepthName;
-            this.region4DepthName = region4DepthName;
-            this.addressDetail = addressDetail;
-            this.longitude = longitude;
-            this.latitude = latitude;
-            this.validateSelf();
-        }
-
-        public static AddressDto fromEntity(Address address) {
-
-            if (address == null) {
-                return null;
-            }
-
-            return AddressDto.builder()
-                    .addressName(address.getAddressName())
-                    .region1DepthName(address.getRegion1DepthName())
-                    .region2DepthName(address.getRegion2DepthName())
-                    .region3DepthName(address.getRegion3DepthName())
-                    .region4DepthName(address.getRegion4DepthName() != null ? address.getRegion4DepthName() : null)
-                    .addressDetail(address.getAddressDetail())
-                    .longitude(address.getLongitude())
-                    .latitude(address.getLatitude())
-                    .build();
-        }
-    }
     public static ReadUserUserDetailResponseDto fromEntity(User user) {
         return ReadUserUserDetailResponseDto.builder()
                 .name(user.getName())
@@ -131,7 +59,7 @@ public class ReadUserUserDetailResponseDto extends SelfValidating<ReadUserUserDe
                 .serialId(user.getSerialId())
                 .phoneNumber(user.getPhoneNumber())
                 .email(user.getEmail() != null ? user.getEmail() : null)
-                .address(user.getAddress() != null ? AddressDto.fromEntity(user.getAddress()) : null)
+                .address(user.getAddress() != null ? AddressResponseDto.fromEntity(user.getAddress()) : null)
                 .build();
     }
 }
