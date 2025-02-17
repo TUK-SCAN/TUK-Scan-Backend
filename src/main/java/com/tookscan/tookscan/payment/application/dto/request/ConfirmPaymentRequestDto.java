54c0a2bdc3c77863d1a6be11e1a3eb602bd19d02
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-02-17 11:49:22 +0900

    feat/#127: Request Body 검증 추가

--- src/main/java/com/tookscan/tookscan/payment/application/dto/request/ConfirmPaymentRequestDto.java
+++ src/main/java/com/tookscan/tookscan/payment/application/dto/request/ConfirmPaymentRequestDto.java
@@ -1,13 +1,17 @@
 package com.tookscan.tookscan.payment.application.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 
 public record ConfirmPaymentRequestDto(
         @JsonProperty("order_number")
+        @NotBlank(message = "주문번호는 필수입니다.")
         String orderNumber,
         @JsonProperty("payment_key")
+        @NotBlank(message = "결제키는 필수입니다.")
         String paymentKey,
         @JsonProperty("amount")
+        @NotBlank(message = "결제금액은 필수입니다.")
         Integer amount
 ) {
 }
