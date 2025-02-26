2190335af2a07b132036ba0cba24f0b917ffd9d8
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-02-26 14:34:06 +0900

    fix/#159: 소셜로그인 시, 리다이렉트 하도록 기능 변경

--- src/main/java/com/tookscan/tookscan/security/handler/login/Oauth2SuccessHandler.java
+++ src/main/java/com/tookscan/tookscan/security/handler/login/Oauth2SuccessHandler.java
@@ -1,5 +1,6 @@
 package com.tookscan.tookscan.security.handler.login;
 
+import com.tookscan.tookscan.core.constant.Constants;
 import com.tookscan.tookscan.core.exception.error.ErrorCode;
 import com.tookscan.tookscan.core.exception.type.CommonException;
 import com.tookscan.tookscan.core.utility.HttpServletUtil;
@@ -33,15 +34,17 @@ public class Oauth2SuccessHandler implements AuthenticationSuccessHandler {
 
         OauthJsonWebTokenDto oauthJsonWebTokenDto = null;
 
+        String redirectPath = (String) request.getSession().getAttribute("REDIRECT_PATH");
+
         if (principal instanceof CustomTemporaryUserPrincipal) {
             oauthJsonWebTokenDto = loginOauthUseCase.execute((CustomTemporaryUserPrincipal) principal);
+            httpServletUtil.onSuccessRedirectResponseWithJWTCookie(Constants.ADDITIONAL_INFO_INPUT_PATH, response, oauthJsonWebTokenDto);
         } else if (principal instanceof CustomUserPrincipal) {
             oauthJsonWebTokenDto = loginOauthUseCase.execute((CustomUserPrincipal) principal);
+            httpServletUtil.onSuccessRedirectResponseWithJWTCookie(redirectPath, response, oauthJsonWebTokenDto);
         }
         else {
             throw new CommonException(ErrorCode.INVALID_PRINCIPAL_TYPE);
         }
-
-        httpServletUtil.onSuccessBodyResponseWithOauthJWTBody(response, oauthJsonWebTokenDto);
     }
 }
